f0c86d05317333716c410a8259fd5f58bafb467d
holman <contact@holmantai.net>
2017-10-16 12:57:24 +0800

    add code

--- src/muti_env.py
+++ src/muti_env.py
@@ -4,7 +4,6 @@ import sys
 
 import cv2
 import matplotlib.pyplot as plt
-
 import gym
 import universe # register the universe environments
 
@@ -12,24 +11,31 @@ from universe import wrappers
 
 import numpy as np
 import tensorflow as tf
-
+import time
 import gym, time, random, threading
 
 from keras.models import *
 from keras.layers import *
 from keras import backend as K
+from keras.models import load_model
 
 logger = logging.getLogger()
 # 'flashgames.NeonRace-v0' 'flashgames.YummyyummyMonsterShooter-v0' #'flashgames.3FootNinja-v0'# 'flashgames.CoasterRacer-v0' #
 ENV = 'flashgames.NeonRace-v0'  # 'flashgames.SuperCarRacing-v0'  # 'flashgames.V8RacingChampion-v0' # 'flashgames.3dSpeedFever-v0'
 THREAD_DELAY = 0.001
-THREADS = 2
-OPTIMIZERS = 1
+THREADS = 4
+OPTIMIZERS = 2
+LOG_FILE_NAME = str(random.randrange(10,1000))+"_reward.log"
 
-RUN_TIME = 600
-EPS_START = 0.4
-EPS_STOP = .15
-EPS_STEPS = 75000
+RUN_TIME = 21600
+EPS_START = 0.5
+EPS_STOP = 0.05
+EPS_STEPS = 375000
+
+
+# EPS_START = 0.000001
+# EPS_STOP = 0.0000001
+# EPS_STEPS = 75000
 
 LEARNING_RATE = 0.005
 MINI_BATCH_SIZE = 32
@@ -37,17 +43,23 @@ MINI_BATCH_SIZE = 32
 GAMMA = 0.99
 N_STEP_RETURN = 8
 GAMMA_N = GAMMA ** N_STEP_RETURN
-NUM_STATE = [396, 622, 3]
+NUM_STATE = [251, 622, 3]
+INPUT_IMG = [84, 206, 3]
+NONE_STATE = np.zeros(INPUT_IMG)
 
 left = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
 right = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', True)]
 Forward = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
-ACTIONS = [left, right, Forward]
+None_key = [('KeyEvent', 'ArrowUp', False), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
+ACTIONS = [left, right, Forward, None_key]
 
+frames = 0
 
 def crop_screen(screen):
     # print(screen.shape)
-    return screen[84:NUM_STATE[0], 18:NUM_STATE[1], :]
+    tmp_img =  screen[229:NUM_STATE[0], 18:NUM_STATE[1], :]
+
+    return cv2.resize(tmp_img, (INPUT_IMG[1], INPUT_IMG[0]), interpolation = cv2.INTER_CUBIC)
 
 
 
@@ -55,7 +67,7 @@ class Brain:
     train_queue = [[], [], [], [], []]  # s, a, r, s', terminal
     lock_queue = threading.Lock()
 
-    def __init__(self, state_size=NUM_STATE, action_space=3):
+    def __init__(self, state_size=INPUT_IMG, action_space=4):
         self.session = tf.Session()
         self.state_size = state_size
         self.action_space = action_space
@@ -75,15 +87,15 @@ class Brain:
     def _build_model(self):
         l_input = Input(batch_shape=(None, self.state_size[0], self.state_size[1], self.state_size[2]))
 
-        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv_1')(l_input)
-        x = MaxPool2D((2, 2), strides=(2, 2), name="max_pool_1")(x)
+        x = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv_1')(l_input)
+        x = MaxPool2D((5, 5), strides=(2, 2), name="max_pool_1")(x)
         x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv_2')(x)
-        x = MaxPool2D((2, 2), strides=(2, 2), name="max_pool_2")(x)
+        x = MaxPool2D((3, 3), strides=(2, 2), name="max_pool_2")(x)
         x = Flatten(name='flatten')(x)
         x = Dense(1028, activation='relu', name='fc1')(x)
         x = Dense(128, activation='relu', name='fc2')(x)
-        out_actions = Dense(self.action_space, activation='softmax')(l_input)
-        out_values = Dense(2, activation='linear')(l_input)
+        out_actions = Dense(self.action_space, activation='softmax')(x)
+        out_values = Dense(1, activation='linear')(x)
 
         model = Model(inputs=[l_input], outputs=[out_actions, out_values])
         # have to init before threading
@@ -117,34 +129,47 @@ class Brain:
 
     def optimize(self):
         if len(self.train_queue[0]) < MINI_BATCH_SIZE :
+            # print("len(self.train_queue[0])", len(self.train_queue[0]))
             time.sleep(0)  # yield
             return
 
         with self.lock_queue:
             if len(self.train_queue[0]) < MINI_BATCH_SIZE:
+                # print(" after lock len(self.train_queue[0])", len(self.train_queue[0]))
                 return
 
             s, a, r, s_, s_mask = self.train_queue
             self.train_queue = [[], [], [], [], []]
-
-        s = np.vstack(s)
+        # for ms in s:
+        #     print(ms.shape)
+        s = np.array(s)
         a = np.vstack(a)
         r = np.vstack(r)
-        s_ = np.vstack(s_)
+        s_ = np.array(s_)
         s_mask = np.vstack(s_mask)
 
         if len(s) > 5 * MINI_BATCH_SIZE:
             print("optimizer alert! with bash size %d" % len(s))
 
         v = self.predict_v(s_)
+        # print(r.shape)
+        # print("v", (v).shape)
+        # print("GAMMA_N * v", (GAMMA_N * v).shape)
+        # print("(GAMMA_N * v * s_mask ).shape", (GAMMA_N * v * s_mask ).shape)
 
         r = r + GAMMA_N * v * s_mask  # set v to 0 where s_ is terminal state
 
         s_t, a_t, r_t, minimize = self.graph
 
+        # print("-------------- mini loss ------------------------")
+        # print(s)
+        # print(a)
+        # print(r.shape)
         self.session.run(minimize, feed_dict={s_t: s, a_t: a, r_t:r})
 
     def train_push(self, s, a, r, s_):
+        if s == None:
+            return
         with self.lock_queue:
             self.train_queue[0].append(s)
             self.train_queue[1].append(a)
@@ -172,36 +197,41 @@ class Brain:
             p, v = self.model.predict(s)
             return v
 
+    def save(self):
+        self.model.save('my_model.h5')
+
+    def load(self):
+        self.model = load_model('my_model.h5')
+
 
 class Agent:
-    def __init__(self, eps_start, eps_end, eps_step, nb_actions=3):
+    def __init__(self, eps_start, eps_end, eps_step, nb_actions=4):
         self.eps_start = eps_start
         self.eps_end = eps_end
         self.eps_step = eps_step
         self.nb_actions = nb_actions
-        self.brain = brain
 
         self.memory = []  # n step return
         self.R = 0.
 
     def getEpsilon(self):
-        if(frames >= eps_step):
+        if(frames >= self.eps_step):
             return self.eps_end
         else:
-            return self.eps_start + frames * (self.eps_end - self.eps_start) / self.eps_steps
+            return self.eps_start + frames * (self.eps_end - self.eps_start) / self.eps_step
         # interpolate linearly
 
     def act(self, s):
         eps = self.getEpsilon()
-        global frames;
+        global frames, brain;
         frames = frames + 1
 
         if random.random() < eps:
             return random.randint(0, self.nb_actions -1)
         else:
             s = np.array([s])
-            p = self.brain.predict_p(s)[0]
-
+            p = brain.predict_p(s)[0]
+            # print(self.nb_actions, p)
             a = np.random.choice(self.nb_actions, p=p)
             return a
 
@@ -217,22 +247,23 @@ class Agent:
         a_cats[a] = 1
 
         self.memory.append((s, a_cats, r, s_))
+
         self.R = (self.R + r * GAMMA_N)/ GAMMA
 
         if s_ is None:
             while len(self.memory) > 0:
                 n = len(self.memory)
                 s, a, r, s_ = get_sample(self.memory, n)
-                self.brain.train_push(s, a, r, s_)
+                brain.train_push(s, a, r, s_)
 
                 self.R = (self.R - self.memory[0][2]) / GAMMA
                 self.memory.pop(0)
 
             self.R = 0
 
-        if len(self.memory) >= N:
+        if len(self.memory) >= N_STEP_RETURN:
             s, a, r, s_ = get_sample(self.memory, N_STEP_RETURN)
-            self.brain_push(s, a, r, s_)
+            brain.train_push(s, a, r, s_)
 
             self.R = self.R - self.memory[0][2]
             self.memory.pop(0)
@@ -244,18 +275,19 @@ class Environment(threading.Thread):
 
     stop_signal = False
 
-    def __init__(self, render=True, eps_start=EPS_START, eps_end=EPS_STOP, eps_steps=EPS_STEPS, nb_actions=3):
+    def __init__(self, render=True, eps_start=EPS_START, eps_end=EPS_STOP, eps_steps=EPS_STEPS, nb_actions=4):
         threading.Thread.__init__(self)
 
         self.render = render
         self.env = gym.make(ENV)
         self.env.configure(remotes=1)
-        # self.agent = Agent(eps_start, eps_end, eps_steps, brain, nb_actions)
+        self.agent = Agent(eps_start, eps_end, eps_steps, nb_actions)
 
     def runEpisode(self):
-        s = self.env.reset()
+        s = self.env.reset()[0]
 
         R = 0
+        start = time.time()
         while True:
             time.sleep(THREAD_DELAY)  # yield
 
@@ -274,18 +306,29 @@ class Environment(threading.Thread):
                 self.env.render()
 
             a = 2
+            if (s != None):
+                a = self.agent.act(s)
+            else:
+                a = 2
             # action_n = [ for ob in s]
             # print(action_n)
             s_, r, done, info = self.env.step([ACTIONS[a]])
-            if (s_[0] == None):
+            s_ = s_[0]
+            done = done[0]
+            if (s_ == None):
+                start = time.time()
                 continue
 
-            s_ = crop_screen(s_[0]['vision'])
+            if(time.time() -start > 160):
+                done = True
+
+            s_ = crop_screen(s_['vision'])
+            # cv2.imwrite('my.jpg', s_)
             # print(done)
 
 
             # terminal state
-            if done[0]:
+            if done:
                 s_ = None
                 print(done, "restart ")
                 self.env.reset()
@@ -296,13 +339,14 @@ class Environment(threading.Thread):
                 break
 
 
-
-            # self.agent.train(s, a, r, s_)
-            print(r, done)
+            self.agent.train(s, a, r[0], s_)
+            # print(r, done)
 
             s = s_
             R += r[0]
 
+        with open(LOG_FILE_NAME,'a') as f:
+            f.write(str(R)+"\n")
         print("Total R:", R)
 
     def run(self):
@@ -321,6 +365,7 @@ class Optimizer(threading.Thread):
 
     def run(self):
         while not self.stop_signal:
+            # print("------------------------opt------------------------")
             brain.optimize()
 
     def stop(self):
@@ -329,11 +374,15 @@ class Optimizer(threading.Thread):
 
 
 
-# brain = Brain()  # brain is global in A3C
+brain = Brain()  # brain is global in A3C
+# brain.load()
 
 def main():
     envs = [Environment() for i in range(THREADS)]
     opts = [Optimizer() for i in range(OPTIMIZERS)]
+
+    for o in opts:
+        o.start()
 
     for e in envs:
         e.start()
@@ -345,7 +394,12 @@ def main():
     for e in envs:
         e.join()
 
-        # return 0
+    for o in opts:
+        o.stop()
+    for o in opts:
+        o.join()
+
+    brain.save()
 
 if __name__ == '__main__':
     main()
